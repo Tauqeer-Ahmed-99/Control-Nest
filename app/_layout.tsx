c2aa6afa22f6c652bdb0945bceda1f48277c792b
NMTSolutions <nanomacrotech.soultions@gmail.com>
2024-09-30 21:23:34 +0530

    added: spash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import "react-native-reanimated";
 
